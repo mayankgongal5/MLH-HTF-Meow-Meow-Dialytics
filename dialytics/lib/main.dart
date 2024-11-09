9ab765be5c56a8c1cdcde6b9d6902c1b5d0a416f
Mayank Ashok Gongal <111949604+mayankgongal5@users.noreply.github.com>
2024-11-09 16:36:39 +0530

    repo

---
+++ dialytics/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+import 'home.dart';
+import 'login_page.dart';
+import 'creds.dart';
+import 'package:supabase/supabase.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: Creds.SUPABASE_URL,
+    anonKey: Creds.SUPABASE_URL,
+  );
+
+  final session = Supabase.instance.client.auth.currentSession;
+
+  runApp(MyApp(startPage: session == null ? LoginPage() : HomePage()));
+}
+
+class MyApp extends StatelessWidget {
+  final Widget startPage;
+
+  MyApp({required this.startPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dialytics',
+      debugShowCheckedModeBanner: false,
+      home: startPage,
+    );
+  }
+}
